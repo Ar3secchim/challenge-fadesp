6456b11471b98e1aad6b0bc7daed420e60915b42
Renara Secchim <renarasecchim@gmail.com>
2025-06-20 06:11:51 -0300

    feat: :sparkles: Initialize Spring Boot project with Maven wrapper, PostgreSQL configuration, and basic application structure

---
+++ src/main/java/com/challenge_fadesp/ChallengeFadespApplication.java
@@ -0,0 +1,13 @@
+package com.challenge_fadesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeFadespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeFadespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge_fadesp/ChallengeFadespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge_fadesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeFadespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
